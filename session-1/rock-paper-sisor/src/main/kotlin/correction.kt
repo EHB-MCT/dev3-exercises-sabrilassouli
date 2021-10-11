d4ee21b2fd477555f9dc7b76aa38ec5b3a7330ee
sabrilassouli <sabri.lassouli@student.ehb.be>
2021-10-11 22:02:12 +0200

    corrected rock-paper-scissors
    wrote the correction to the exercise rock-paper-scissors

--- session-1/rock-paper-sisor/src/main/kotlin/correction.kt
+++ session-1/rock-paper-sisor/src/main/kotlin/correction.kt
@@ -1,3 +1,35 @@
 fun main(){
-    v
+    val options = arrayOf("rock", "paper", "scissors")
+    val computerChoice = options.random()
+    val userChoice = getUserChoice(options)
+
+    println("computer picked $computerChoice")
+    println("user picked $userChoice")
+    determineWinner(computerChoice, userChoice)
+}
+
+fun getUserChoice(options: Array<String>): String? {
+    var userChoice: String?
+    do{
+        println("make your choice: rock - paper - scissors")
+        userChoice = readLine()
+    } while (!options.contains(userChoice))
+    return userChoice
+}
+
+fun determineWinner(computerChoice: String, userChoice: String?){
+    if (computerChoice == userChoice){
+        println("it's a draw")
+        return
+    }
+
+    val winMessage = "you've won"
+    val loseMessage = "you've lost"
+
+    when (computerChoice) {
+        "rock" -> if (userChoice == "paper") println(winMessage) else println(loseMessage)
+        "paper" -> if (userChoice == "scissors") println(winMessage) else println(loseMessage)
+        "scissors" -> if (userChoice == "rock") println(winMessage) else println(loseMessage)
+
+    }
 }
